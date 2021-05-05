b232ac0ad459e908eff01d3f7433bb101b9eda50
sklyaba <sklyaba@ya.ru>
2021-05-05 22:12:34 +0300

    task5: solve min max definitions -> next right & left position moving

--- src/main/java/com/dan/task5/Task5.java
+++ src/main/java/com/dan/task5/Task5.java
@@ -10,22 +10,27 @@ public class Task5 {
         char[] chars = SEATS.get(0).toCharArray();
 
         Period seat = new Period();
-        if (chars[0] == 'F') {
+        if (chars[0] == 'F') { // 0 --- 127
             seat.maxRow /= 2;
         } else seat.minRow = seat.maxRow / 2;
+        for (int i = 1; i < chars.length; i++) {
+            if (chars[i] == 'F') {
+//                int in = (seat.maxRow - seat.minRow) / 2;
+                seat.maxRow -= (seat.maxRow - seat.minRow) / 2;
+            }
+            if (chars[i] == 'B') {
+//                int in = (seat.maxRow - seat.minRow) / 2;
+                seat.minRow += (seat.maxRow - seat.minRow) / 2;
+            }
+        }
 
         System.out.println(seat);
-//        for (int i = 1; i < chars.length; i++) {
-//            if (chars[i] == 'F') {
-//
-//            }
-//
-//        }
+
     }
 
     static class Period {
         int minRow = 0;
-        int maxRow = 127;
+        int maxRow = 126;
         int minSeat = 0;
         int maxSeat = 7;
 
